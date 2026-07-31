05d08e540005ec5fcb245734070c84388c0ac7e2
Rouse <ro2056ss>
2026-07-30 23:36:55 -0500

    Initial Commit

---
+++ veterinariaPatitas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  veterinariaPatitas
+//
+//  Created by Rouse on 30/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
